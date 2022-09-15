25664bf24bf3f80b339decb17da9abe89a5770e3
Reagan Ekhameye <reaganekhameye@gmail.com>
2022-09-15 10:18:24 +0100

    feat: change folder structure

---
+++ components/users/users.routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const {
+	getUsers,
+	getUser,
+	createUser,
+	updateUser,
+	deleteUser,
+} = require('./users.controller')
+
+const User = require('./user.model')
+
+const router = express.Router({ mergeParams: true })
+
+const advancedResults = require('../../middleware/advancedResults')
+const { protect, authorize } = require('../../middleware/auth')
+
+router.use(protect)
+router.use(authorize('admin'))
+
+router.route('/').get(advancedResults(User), getUsers).post(createUser)
+
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser)
+
+module.exports = router
