ba8ea7822b69a121fd87c3590ed452ee276c7315
Reagan Ekhameye <reaganekhameye@gmail.com>
2022-09-24 21:13:30 +0100

    refactor: modify error response to use typescript

--- basic-typescript/src/utils/errorResponse.ts
+++ basic-typescript/src/utils/errorResponse.ts
@@ -1,15 +1,9 @@
 export default class ErrorResponse extends Error {
-	public statusCode: number
-	public messageWithField: null | object
-
 	constructor(
 		message: string | null,
-		statusCode: number,
-		messageWithField: null | object = null
+		public statusCode: number | null = null,
+		public messageWithField: null | object = null
 	) {
 		super(message === null ? '' : message)
-
-		this.statusCode = statusCode
-		this.messageWithField = messageWithField
 	}
 }
