30c132ac4f4dc33ef8a242fe5eaf4792ec808c0f
Reagan Ekhameye <reaganekhameye@gmail.com>
2022-09-24 21:11:45 +0100

    refactor: modify require to es6 module syntax

--- basic-typescript/src/routes.ts
+++ basic-typescript/src/routes.ts
@@ -4,7 +4,7 @@ import userRoutes from './components/users/users.routes'
 
 const versionOne = (routeName: string) => `/api/v1/${routeName}`
 
-module.exports = (app: Express) => {
+export default (app: Express) => {
 	app.use(versionOne('auth'), authRoutes)
 	app.use(versionOne('users'), userRoutes)
 }
